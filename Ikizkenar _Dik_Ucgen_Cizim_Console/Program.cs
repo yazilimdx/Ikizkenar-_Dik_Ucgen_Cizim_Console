4882e61f943ad9e8308cc203d54b5696b38ee14c
yazilimdx <76188846+yazilimdx@users.noreply.github.com>
2022-05-07 18:57:31 +0300

    Initial commit

---
+++ Ikizkenar _Dik_Ucgen_Cizim_Console/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ikizkenar__Dik_Ucgen_Cizim_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Yukseklik giriniz:");
+            int h = Convert.ToInt32(Console.ReadLine());
+            UcgenCiz(h);
+            Console.ReadKey();
+        }
+
+
+       static   void UcgenCiz(int height)
+        {
+            for (int i = 1; i <= height; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Ikizkenar-_Dik_Ucgen_Cizim_Console
+ Kullanicidan alinan sayi degerinde yukseklige  sahip dik ucgeni  cizer.
